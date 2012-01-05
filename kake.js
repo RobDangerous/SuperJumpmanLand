57717a224a631f52871afda98c43411e9bab6964
Robert Konrad <robert@ktx-software.com>
2012-01-05 01:37:41 +0100

    Added hxcpp base-lib.

--- kake.js
+++ kake.js
@@ -3,10 +3,10 @@ importPackage(com.ktxsoftware.kake);
 var solution = new Solution("SML");
 var project = new Project("SML");
 
-project.addFiles("Sources/**.h", "Sources/**.cpp", "haXe/**.h", "haXe/**.cpp");
-project.addIncludeDir("C:/Motion-Twin/haxe/lib/hxcpp/2,08,0");
+project.addFiles("Sources/**.h", "Sources/**.cpp", "haXe/**.h", "haXe/**.cpp", "hxcpp/**.h", "hxcpp/**.cpp");
+project.addIncludeDirs("hxcpp/include", "haXe/include");
 project.setDebugDir("Deployment");
-
+project.addDefine("HX_WINDOWS");
 project.addSubProject(Solution.createProject("Kt"));
 
 solution.addProject(project);
