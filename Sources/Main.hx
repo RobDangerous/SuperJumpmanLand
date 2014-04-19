646c1d8a9022034bb35f6606894e673ac65628ee
Robert Konrad <robert@ktx-software.com>
2014-04-19 16:28:06 +0200

    Kha update.

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -1,9 +1,10 @@
-package ;
+package;
 
 import kha.Starter;
 
 class Main {
 	public static function main() {
-		new Starter().start(new SuperMarioLand());
+		var starter = new Starter();
+		starter.start(new SuperMarioLand());
 	}
 }
